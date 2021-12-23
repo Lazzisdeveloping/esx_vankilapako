9ccf0a071add4ce5acf06bf58c3fb4c1efb46228
Lazzisdeveloping <79515120+Lazzisdeveloping@users.noreply.github.com>
2021-12-24 00:18:42 +0200

    Add files via upload

---
+++ esx_vankilapako/config.lua
@@ -0,0 +1,22 @@
+Config = {}
+Config.Locale = 'fi'
+
+Config.Marker = {
+	r = 250, g = 0, b = 0, a = 100,  
+	x = 1.0, y = 1.0, z = 1.5,       
+	DrawDistance = 15.0, Type = 1    
+}
+
+Config.boliisia = 1 --Monta poliisia tarvitaan aloitukseen
+Config.eijaksaoottaa    = 100 -- kauan joutuu odottamaan uutta hakkerointi yritystä [sekunteina]
+
+Config.MaxDistance    = 1  
+
+Stores = {
+	["Vankila"] = {
+		position = { x = 1831.36, y = 2603.34, z = 45.89 },
+		secondsRemaining = 3, --hakkeroinnin pituus [sekunteina]
+		lastRobbed = 0
+	},
+
+}
---
+++ esx_vankilapako/locales/fi.lua
@@ -0,0 +1,15 @@
+Locales['fi'] = {
+  ['press_to_rob'] = 'paina ~INPUT_CONTEXT~ ~o~hakkeroituaksesi~s~ vankilan tietokantaan',
+  ['robbery_timer'] = 'Hakkeroidaan... ~r~%s~s~ sekunttia jäljellä',
+  ['rob_in_prog'] = '~r~Hälytys kohteessa vankila',
+  ['started_to_rob'] = 'sinä aloitit vankilan ~y~hakkeroinnin',
+  ['alarm_triggered'] = 'hälyytys laukaistu!',
+  ['robbery_complete_at'] = '~r~Hälytys ohitse kohteessa vankila~s~',
+  ['robbery_cancelled'] = 'Hakkerointi keskeytyi!',
+  ['robbery_cancelled_at'] = '~r~Hälytys kohteessa ~b~vankila~s~ keskeytyi!',
+  ['min_police'] = 'kaupungissa pitää olla vähintää ~b~%s poliisia~s~ paikalla hakkeroinnin aloitukseen.',
+  ['robbery_already'] = '~r~Hakkerointi on jo menossa.',
+  ['no_threat'] = 'Sinulla tulee olla ase aloittaaksesi',
+  ['recently_hacked'] = 'Et voi aloittaa uutta hakkerointia vielä',
+
+}
