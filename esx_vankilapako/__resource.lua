a7250da29521d31c23e6c63c633231d92e0d2690
Lazzis <79515120+Lazzisdeveloping@users.noreply.github.com>
2021-12-26 06:28:59 +0200

    Update __resource.lua

--- esx_vankilapako/__resource.lua
+++ esx_vankilapako/__resource.lua
@@ -6,26 +6,14 @@ version '1.1.0'
 
 client_scripts {
 	'@es_extended/locale.lua',
-	'locales/de.lua',
-	'locales/en.lua',
 	'locales/fi.lua',
-	'locales/fr.lua',
-	'locales/sv.lua',
-	'locales/pl.lua',
-	'locales/de.lua',
 	'config.lua',
 	'client/main.lua'
 }
 
 server_scripts {
 	'@es_extended/locale.lua',
-	'locales/de.lua',
-	'locales/en.lua',
 	'locales/fi.lua',
-	'locales/fr.lua',
-	'locales/sv.lua',
-	'locales/pl.lua',
-	'locales/de.lua',
 	'config.lua',
 	'server/main.lua'
 }
