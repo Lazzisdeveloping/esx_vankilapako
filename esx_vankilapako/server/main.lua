941fca878c013632c6bcf953b0c5b79b869a130e
Lazzis <79515120+Lazzisdeveloping@users.noreply.github.com>
2021-12-26 06:30:03 +0200

    Update main.lua

--- esx_vankilapako/server/main.lua
+++ esx_vankilapako/server/main.lua
@@ -5,7 +5,7 @@ ESX = nil
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
 RegisterServerEvent('esx_vankilapako:tooFar')
-AddEventHandler('esx_vankilapako:tooFar', function(currentStore)
+AddEventHandler('esx_vankilapako:tooFar', function(currentjail)
 	local _source = source
 	local xPlayers = ESX.GetPlayers()
 	rob = false
@@ -14,7 +14,7 @@ AddEventHandler('esx_vankilapako:tooFar', function(currentStore)
 		local xPlayer = ESX.GetPlayerFromId(xPlayers[i])
 		
 		if xPlayer.job.name == 'police' or xPlayer.job.name == 'sheriff' then
-			TriggerClientEvent('esx:showNotification', xPlayers[i], _U('robbery_cancelled_at', Stores[currentStore].nameOfStore))
+			TriggerClientEvent('esx:showNotification', xPlayers[i], _U('robbery_cancelled_at', jails[currentjail].nameOfjail))
 			TriggerClientEvent('esx_vankilapako:killBlip', xPlayers[i])
 		end
 	end
@@ -22,21 +22,21 @@ AddEventHandler('esx_vankilapako:tooFar', function(currentStore)
 	if robbers[_source] then
 		TriggerClientEvent('esx_vankilapako:tooFar', _source)
 		robbers[_source] = nil
-		TriggerClientEvent('esx:showNotification', _source, _U('robbery_cancelled_at', Stores[currentStore].nameOfStore))
+		TriggerClientEvent('esx:showNotification', _source, _U('robbery_cancelled_at', jails[currentjail].nameOfjail))
 	end
 end)
 
 RegisterServerEvent('esx_vankilapako:robberyStarted')
-AddEventHandler('esx_vankilapako:robberyStarted', function(currentStore)
+AddEventHandler('esx_vankilapako:robberyStarted', function(currentjail)
 	local _source  = source
 	local xPlayer  = ESX.GetPlayerFromId(_source)
 	local xPlayers = ESX.GetPlayers()
 
-	if Stores[currentStore] then
-		local store = Stores[currentStore]
+	if jails[currentjail] then
+		local jail = jails[currentjail]
 
-		if (os.time() - store.lastRobbed) < Config.eijaksaoottaa and store.lastRobbed ~= 0 then
-			TriggerClientEvent('esx:showNotification', _source, _U('recently_hacked', Config.eijaksaoottaa - (os.time() - store.lastRobbed)))
+		if (os.time() - jail.ronklattu) < Config.eijaksaoottaa and jail.ronklattu ~= 0 then
+			TriggerClientEvent('esx:showNotification', _source, _U('recently_hacked', Config.eijaksaoottaa - (os.time() - jail.ronklattu)))
 			return
 		end
 
@@ -49,26 +49,31 @@ AddEventHandler('esx_vankilapako:robberyStarted', function(currentStore)
 		end
 
 		if not rob then
-			if cops >= Config.boliisia then
+			if xPlayer.getInventoryItem('hakkerointilaite').count >= 1 then
+				if cops >= Config.boliisia then
+
 				rob = true
+
+				xPlayer.removeInventoryItem('hakkerointilaite', 1)
 
 				for i=1, #xPlayers, 1 do
 					local xPlayer = ESX.GetPlayerFromId(xPlayers[i])
 					if xPlayer.job.name == 'police' or xPlayer.job.name == 'sheriff' then
-						TriggerClientEvent('esx_vankilapako:setBlip', xPlayers[i], Stores[currentStore].position)
+						TriggerClientEvent('esx_vankilapako:setBlip', xPlayers[i], jails[currentjail].sijainti)
 					end
 				end
 
-				TriggerClientEvent('esx:showNotification', _source, _U('started_to_rob', store.nameOfStore))
+				TriggerClientEvent('esx:showNotification', _source, _U('started_to_rob', jail.nameOfjail))
 				TriggerClientEvent('esx:showNotification', _source, _U('alarm_triggered'))
+				TriggerClientEvent('esx_vankilapako:anim', _source)
 				
-				TriggerClientEvent('esx_vankilapako:currentlyRobbing', _source, currentStore)
+				TriggerClientEvent('esx_vankilapako:currentlyRobbing', _source, currentjail)
 				TriggerClientEvent('esx_vankilapako:startTimer', _source)
 				
-				Stores[currentStore].lastRobbed = os.time()
-				robbers[_source] = currentStore
+				jails[currentjail].ronklattu = os.time()
+				robbers[_source] = currentjail
 
-				SetTimeout(store.secondsRemaining * 1000, function()
+				SetTimeout(jail.aika * 1000, function()
 					if robbers[_source] then
 						rob = false
 						if xPlayer then
@@ -79,7 +84,7 @@ AddEventHandler('esx_vankilapako:robberyStarted', function(currentStore)
 								xPlayer = ESX.GetPlayerFromId(xPlayers[i])
 
 								if xPlayer.job.name == 'police' or xPlayer.job.name == 'sheriff' then
-									TriggerClientEvent('esx:showNotification', xPlayers[i], _U('robbery_complete_at', store.nameOfStore))
+									TriggerClientEvent('esx:showNotification', xPlayers[i], _U('robbery_complete_at', jail.nameOfjail))
 									TriggerClientEvent('esx_vankilapako:killBlip', xPlayers[i])
 								end
 							end
@@ -89,6 +94,9 @@ AddEventHandler('esx_vankilapako:robberyStarted', function(currentStore)
 			else
 				TriggerClientEvent('esx:showNotification', _source, _U('min_police', Config.boliisia))
 			end
+		else
+			TriggerClientEvent('esx:showNotification', _source, ('Sinulla ei ole ~r~hakkerointilaitetta'))
+		end
 		else
 			TriggerClientEvent('esx:showNotification', _source, _U('robbery_already'))
 		end
